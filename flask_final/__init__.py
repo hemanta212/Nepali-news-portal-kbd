d1bc44bca62ca8adc9d6f71aa71cff12f3203aac
hemanta212 <hemantasharma.212@gmail.com>
2018-10-29 16:19:15 +0545

    fixed security issues

--- flask_final/__init__.py
+++ flask_final/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,17 +11,16 @@ login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
 
-app.config["SECRET_KEY"] = "alsdkjf34lkj3klkjdfl3kjlkj45kljldkfjlf"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] ='sharmahemanta.212@gmail.com'
-app.config['MAIL_PASSWORD'] ='9849118360Hs'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
-
 
 
 from flask_final import routes
