ece1f586c12810921b1badac00149b5e29da3662
hemanta212 <sharmahemanta.212@gmail.com>
2019-02-02 18:33:17 +0545

    temp fix

--- flask_final/__init__.py
+++ flask_final/__init__.py
@@ -27,7 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
