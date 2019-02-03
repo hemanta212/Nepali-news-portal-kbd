13e8c70af9fb7a27d263d38f2a101d852e34bdae
hemanta212 <sharmahemanta.212@gmail.com>
2019-02-03 21:51:23 +0545

    fixed try option bug and made offline friendly dev possible

--- flask_final/utils.py
+++ flask_final/utils.py
@@ -1,22 +1,28 @@
-from flask_final.nagarik_international import nagarik_international_extractor
-from flask_final.kantipur_daily import kantipur_daily_extractor
-from flask_final.kathmandupost import kathmandu_post_extractor
-from flask_final.models import NepNationalNews as NNN
-from flask_final.models import NepInternationalNews as NIN
-from flask_final.models import EngNationalNews as ENN
-from flask_final import db
+try:
+    from flask_final.nagarik_international import nagarik_international_extractor
+    from flask_final.kantipur_daily import kantipur_daily_extractor
+    from flask_final.kathmandupost import kathmandu_post_extractor
 
+except Exception as e:
+    print(e)
 
-def news_fetcher(category):
-    category_dict = {
-        NNN: kantipur_daily_extractor(),
-        NIN: nagarik_international_extractor(),
-        ENN: kathmandu_post_extractor(),
-    }
-    print("I am runing after declaration of dict")
-    raw_news_list = category_dict[category]
-    print("I am runing after declaration of raw_news_list")
-    try:
+    def news_fetcher(category):
+        pass
+
+else:
+    from flask_final.models import NepNationalNews as NNN
+    from flask_final.models import NepInternationalNews as NIN
+    from flask_final.models import EngNationalNews as ENN
+    from flask_final import db
+
+    def news_fetcher(category):
+        category_dict = {
+            NNN: kantipur_daily_extractor(),
+            NIN: nagarik_international_extractor(),
+            ENN: kathmandu_post_extractor(),
+        }
+        raw_news_list = category_dict[category]
+
         for news in raw_news_list[::-1]:
             dup = category.query.filter_by(title=news["title"]).first()
 
@@ -32,5 +38,3 @@ def news_fetcher(category):
         for i in category.query.order_by(category.date.asc())[30:]:
             db.session.delete(i)
             db.session.commit()
-    except:
-        pass
