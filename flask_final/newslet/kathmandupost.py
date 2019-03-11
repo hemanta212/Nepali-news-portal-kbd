a274efcb221cbdc86332f25db4155dbc787a59b1
hemanta212 <sharmahemanta.212@gmail.com>
2019-03-11 22:54:55 +0545

    Added more sources to kathmandupost extractor

--- flask_final/newslet/kathmandupost.py
+++ flask_final/newslet/kathmandupost.py
@@ -18,10 +18,10 @@ soup = BS(page.content, 'lxml')
 
 
 def featured():
-    featured_news = []
-    sticky_news = soup.find_all('div', class_="sticky-news")
+    featured_news_list = []
+    sticky_news_list = soup.find_all('div', class_="sticky-news")
 
-    for news in sticky_news:
+    for news in sticky_news_list:
         img_div = news.find('div', class_='image')
         try:
             img_link = img_div.img['data-original']
@@ -55,59 +55,22 @@ def featured():
             "image_link": img_link,
         }
 
-        featured_news.append(news_dict)
+        featured_news_list.append(news_dict)
 
-    return featured_news
+    return featured_news_list
+
 
 def kathmandu_post_extractor():
-    main_news = soup.find('div', class_="main-news")
-    news_list = main_news.find_all('div', class_="item")
-    main_list = []
-
-    for news in news_list:
-        post_link = news.h2.a['href']
-        default_link = "https://kathmandupost.ekantipur.com"
-        full_link = default_link + post_link
-        title = news.h2.a.text
-        image_div = news.find('div', class_='ktp-main-news')
-        if image_div is None:
-            continue
-        else:
-            try:
-                image_link = image_div.img['data-original']
-            except:
-                image_link = "img not available"
-
-        # dealing with variable date location during parsing.
-        try:
-            date = news.find('div', class_="post").text.split(", ")[
-                2].rstrip()
-        except IndexError:
-            date = news.find('div', class_="post").text.split(", ")[
-                1].rstrip()
-
-        summary = news.find('div', class_="text").text
-        news_dict = {
-            "image_link": image_link,
-            "title": title,
-            "nep_date": date,
-            "source": "ekantipur",
-            "news_link": full_link,
-            "summary": summary,
-        }
-        main_list.append(news_dict)
-        last_list = featured() + main_list
-
-    return last_list
-
-def testing():
-    main_news_div1 = soup.find('div', class_="news")
+    main_news_div1 = soup.find('div', class_="main-news")
     main_news_div2 = soup.find('div', class_="news")
-    sources = [main_news_div1, main_news_div2]
-    main_list = []
+    main_news_div3_pre = soup.find('div', class_="home-featured-news")
+    main_news_div3 = main_news_div3_pre.find('div', class_='newslist')
+
+    sources = [main_news_div1, main_news_div2, main_news_div3]
+    main_news_list = []
+
     for i in sources:
         news_list = i.find_all('div', class_="item")
-        #[print(i.find('div', class_="text").text) for i in news_list]
         for news in news_list:
             post_link = news.h2.a['href']
             default_link = "https://kathmandupost.ekantipur.com"
@@ -122,11 +85,14 @@ def testing():
 
             # dealing with variable date location during parsing.
             try:
-                date = news.find('div', class_="post").text.split(", ")[
+                date = news.find('div', class_="post").text.split(",")[
                     2].rstrip()
             except IndexError:
-                date = news.find('div', class_="post").text.split(", ")[
-                    1].rstrip()
+                try:
+                    date = news.find('div', class_="post").text.split(",")[
+                        1].rstrip()
+                except IndexError:
+                    date = "error"
 
             summary = news.find('div', class_="text").text
             news_dict = {
@@ -137,10 +103,12 @@ def testing():
                 "news_link": full_link,
                 "summary": summary,
             }
-            main_list.append(news_dict)
+            main_news_list.append(news_dict)
 
-    print(len(main_list))
-    return main_list
+    all_news_list = featured() + main_news_list
+    print(len(all_news_list))
+    pprint(all_news_list)
+    return all_news_list
 
 if __name__ == "__main__":
-    testing()
+    kathmandu_post_extractor()
