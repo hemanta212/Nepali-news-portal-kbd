10cf032a6d11e79e083906120361bab355f2795d
hemanta212 <sharmahemanta.212@gmail.com>
2019-03-25 18:42:48 +0545

    moved manage.py out

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import ProductionConfig
+from flask_final.config import ProductionConfig
 from flask_final import app, db
 
 app.config.from_object(ProductionConfig)
