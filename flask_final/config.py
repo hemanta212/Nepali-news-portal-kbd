8514338347bb9e12c86f4b416981e640ef1f4bb9
hemanta212 <sharmahemanta.212@gmail.com>
2019-03-25 18:47:04 +0545

    fixed Config imports

--- flask_final/config.py
+++ flask_final/config.py
@@ -8,7 +8,7 @@ class Config:
     # app.config["SECRET_KEY"] = configvar["SECRET_KEY"]
     # use os.environ.get() if not linux server
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from flask_final.config import ProductionConfig
+from flask_final.config import Config
 from flask_final import app, db
 
-app.config.from_object(ProductionConfig)
+app.config.from_object(Config)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
